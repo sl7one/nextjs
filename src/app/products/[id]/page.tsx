7e55e53e6d15dcb794c4b462ad7f4cf35e04f2ae
Slava <110696118+sl7one@users.noreply.github.com>
2023-09-16 22:26:39 +0300

    upd

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,30 +1,29 @@
 import Article from "@/components/Article/Article";
 import React from "react";
 
-// export async function generateStaticParams() {
-//   const { products } = await fetch("https://dummyjson.com/products/").then(
-//     res => res.json()
-//   );
-
-//   return products.map(({ title }: { title: string }) => ({
-//     title: title.replace(/\s+/g, "-"),
-//   }));
-// }
-
 interface IProps {
   params: {
     id: string;
   };
 }
 
+// export async function generateStaticParams(props) {
+//   console.log(props)
+//   const { products } = await fetch("https://dummyjson.com/products/").then(
+//     res => res.json()
+//   );
+//   const data = products.map(({ id }: { id: string }) => ({
+//     id,
+//   }));
+
+//   return data;
+// }
+
 export default async function Product({ params: { id } }: IProps) {
   const product: IProduct = await fetch(
-    `https://dummyjson.com/products/${id}`
+    `https://dummyjson.com/products/${id}`,
+    { cache: "no-store" }
   ).then(res => res.json());
 
-  return (
-    <div>
-      <Article product={product} />
-    </div>
-  );
+  return <div>{<Article product={product} />}</div>;
 }
